5af5cebbac055d232b271700b76932344fcf6126
Jan Starý <hans@stare.cz>
2018-11-05 18:45:21 +0100

    use the err() and strtonum() we tested for

--- prog.c
+++ prog.c
@@ -1,8 +1,44 @@
+#include "config.h"
 #include "prog.h"
 
-int
-main()
+#include <stdint.h>
+#include <stdio.h>
+
+#ifdef HAVE_ERR
+#include <err.h>
+#endif
+
+extern const char* __progname;
+
+static void
+usage()
 {
-	/* FIXME: use everything we test for. */
+	warnx("usage: %s number", __progname);
+}
+
+int
+main(int argc, char** argv)
+{
+	uint8_t i;
+	const char *e = NULL;
+
+/* FIXME: use everything we tested for.
+have-gethostbyname.c
+have-msgcontrol.c
+have-reallocarray.c
+have-sndfile.c
+have-socket.c
+have-strlcat.c
+have-strlcpy.c
+*/
+	if (--argc < 1) {
+		usage();
+		return 1;
+	}
+
+	i = strtonum(*++argv, 0, 255, &e);
+	if (e != NULL)
+		warnx("%s is %s", *argv, e);
+
 	return 0;
 }
